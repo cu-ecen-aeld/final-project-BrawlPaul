bc6e5e809424bb2bce7c634303f4453861944b91
Benjamin Lappinga <benjaminl746@gmail.com>
2026-08-08 17:21:33 -0700

    building ssd1306

---
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -0,0 +1,31 @@
+
+##############################################################
+#
+# AESD-ASSIGNMENTS
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_ASSIGNMENTS_DEPENDENCIES = host-dtc
+AESD_ASSIGNMENTS_VERSION = 5f18c78f12fde8541f08a62f1953c5fbcbbf4911
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_ASSIGNMENTS_SITE = git@github.com:BrawlPaul/ECEA-5307-FinalProject.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) ARCH=arm64 CROSS_COMPILE=$(TARGET_CROSS) KERNELDIR=$(LINUX_DIR) -C $(@D)/ssd1306 all
+	$(HOST_DIR)/bin/dtc -@ -I dts -O dtb -o $(@D)/ssd1306/ssd1306.dtbo $(@D)/ssd1306/ssd1306.dts
+
+endef
+
+define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CROSS) -C $(LINUX_DIR) M=$(@D)/ssd1306 modules_install INSTALL_MOD_PATH=$(TARGET_DIR)
+	mkdir -p $(BINARIES_DIR)/rpi-firmware/overlays
+    cp -f $(@D)/ssd1306/ssd1306.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
+	
+endef
+
+$(eval $(generic-package))
