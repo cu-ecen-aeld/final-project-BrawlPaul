a06e8c51f2b06401d7cb33634b03a9ae3961e749
Benjamin Lappinga <benjaminl746@gmail.com>
2026-08-12 21:56:18 -0700

    added dpad integration

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -18,13 +18,18 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) ARCH=arm64 CROSS_COMPILE=$(TARGET_CROSS) KERNELDIR=$(LINUX_DIR) -C $(@D)/ssd1306 all
 	$(HOST_DIR)/bin/dtc -@ -I dts -O dtb -o $(@D)/ssd1306/ssd1306.dtbo $(@D)/ssd1306/ssd1306.dts
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) ARCH=arm64 CROSS_COMPILE=$(TARGET_CROSS) KERNELDIR=$(LINUX_DIR) -C $(@D)/dpad all
+	$(HOST_DIR)/bin/dtc -@ -I dts -O dtb -o $(@D)/dpad/dpad.dtbo $(@D)/dpad/dpad.dts
 
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CROSS) -C $(LINUX_DIR) M=$(@D)/ssd1306 modules_install INSTALL_MOD_PATH=$(TARGET_DIR)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=$(TARGET_CROSS) -C $(LINUX_DIR) M=$(@D)/dpad modules_install INSTALL_MOD_PATH=$(TARGET_DIR)
 	mkdir -p $(BINARIES_DIR)/rpi-firmware/overlays
     cp -f $(@D)/ssd1306/ssd1306.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
+	cp -f $(@D)/dpad/dpad.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
+
 	
 endef
 
